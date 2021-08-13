34fa66b4998e362a97ea8735fcc3b7b046ac9a3e
Lindsay Gillespie <lind-gillespie@sbcglobal.net>
2021-08-13 12:00:59 -0500

    Updated number validation so it cant be blank

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -14,7 +14,8 @@ public class Customer {
 	
 	@Min(value =0, message="must be greater than or equal to zero")
 	@Max(value =10, message="must be less than or equal to ten")
-	private int freePasses;
+	@NotNull(message="is required")
+	private Integer freePasses;
 	
 	public String getFirstName() {
 		return firstName;
@@ -28,10 +29,10 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 }
